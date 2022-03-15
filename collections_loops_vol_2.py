2d8986f6e83510c5e0a23e4d9305cbe018c67fb8
Grzegorz Wojdyga <g.wojdyga@gmail.com>
2022-03-15 16:13:33 +0100

    pierwsze pliki

---
+++ collections_loops_vol_2.py
@@ -0,0 +1,74 @@
+def task15():
+    """
+    Function that accepts a comma separated sequence of words as input and prints the words in a comma-separated
+    sequence after sorting them alphabetically.
+    """
+    pass
+
+
+def task16():
+    """
+    Function that compute the frequency of the words from the input. The output should output after sorting the key
+    alphanumerically.
+    Input: none
+    Output: word -> count for every word in separated lines
+    """
+    pass
+
+
+def task17():
+    """
+    Function that take sentence with words and numbers and calculate the number of letters and digits
+    Input: sentence
+    Output: dictionary (e.g. {"DIGITS": 3, "LETTERS":65})
+    """
+    pass
+
+
+def task18():
+    """
+    Function that take sentence with words and numbers and calculate the number upper case letters and lower case letters
+    
+    Input: sentence
+    Output: dictionary (e.g. {"UPPER": 3, "LOWER":65})
+    """
+    pass
+
+
+def task19():
+    """
+    Function that take sentence with words and numbers and calculate the number upper case and lower case letters
+
+    Input: sentence
+    Output: dictionary (e.g. {"UPPER": 3, "LOWER":65})
+    """
+    pass
+
+
+def task20():
+    """
+    Function that square each odd number in a list
+    Use filer, labmda and map.
+
+    Input: sentence with numbers that are comma separated (e.g. 5,6,7,8)
+    Output: sentence with numbers (e.g. 25,49)
+    """
+    pass
+
+
+def task21():
+    """
+    Function that computes the net amount of a bank account based a transaction log. The transaction log format is shown as following:
+    M 100
+    W 200
+    for M-more and L-less
+
+    Input: Log
+    e.g.
+    M 400
+    M 300
+    L 100
+
+    Output: number (e.g. 600)
+    """
+    pass
